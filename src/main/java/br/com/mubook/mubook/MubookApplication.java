f270667bd9504c7b62fe8661ef734d1a1134d198
JoaoGuilhermeMA <joomoreira36@gmail.com>
2025-05-20 21:32:23 -0300

    iniciando o projeto

---
+++ src/main/java/br/com/mubook/mubook/MubookApplication.java
@@ -0,0 +1,13 @@
+package br.com.mubook.mubook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MubookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MubookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mubook/mubook/MubookApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mubook.mubook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MubookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
