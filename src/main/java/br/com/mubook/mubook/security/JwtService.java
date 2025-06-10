4c05f5e48ffa7fbc7c57fd4e30b97f5f2f90eaff
Vinicius de Oliveira <viniciusmgomes2005@hotmail.com>
2025-06-10 18:13:12 -0300

    fix: corrige anotação value

--- src/main/java/br/com/mubook/mubook/security/JwtService.java
+++ src/main/java/br/com/mubook/mubook/security/JwtService.java
@@ -6,7 +6,7 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import jakarta.annotation.PostConstruct;
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
