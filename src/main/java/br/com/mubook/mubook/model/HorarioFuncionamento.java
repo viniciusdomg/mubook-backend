1b234ee978aef050b24fab559aaf4b7bd29d36af
JoaoGuilhermeMA <joomoreira36@gmail.com>
2025-07-02 17:40:35 -0300

    feat: criando o model e o entity de HorarioFUncionamento

---
+++ src/main/java/br/com/mubook/mubook/model/HorarioFuncionamento.java
@@ -0,0 +1,27 @@
+package br.com.mubook.mubook.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class HorarioFuncionamento {
+    private DayOfWeek diaSemana;
+    private LocalTime horarioAbertura;
+    private LocalTime horarioFechamento;
+    private TipoQuadra tipoQuadra;
+
+    public boolean validarHorario(LocalDateTime horario) {
+        DayOfWeek dia = horario.getDayOfWeek();
+        LocalTime hora = horario.toLocalTime();
+        return dia.equals(diaSemana)
+                && (hora.equals(horarioAbertura) || hora.equals(horarioFechamento)
+                || (hora.isAfter(horarioAbertura) && hora.isBefore(horarioFechamento)));
+    }
+}
