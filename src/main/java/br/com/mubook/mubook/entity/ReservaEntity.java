4dae8bb86a680413cfaa68aceb94ba8153d38130
JoaoGuilhermeMA <joomoreira36@gmail.com>
2025-07-03 20:44:46 -0300

    feact: criando o model e o entity de reserva

---
+++ src/main/java/br/com/mubook/mubook/entity/ReservaEntity.java
@@ -0,0 +1,41 @@
+package br.com.mubook.mubook.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import br.com.mubook.mubook.enums.StatusReserva;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "reserva")
+public class ReservaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "data_hora", nullable = false)
+    private LocalDateTime dataHora;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id", nullable = false)
+    private UsuarioEntity usuario;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private StatusReserva status;
+
+    @ManyToOne
+    @JoinColumn(name = "quadra_id", nullable = false)
+    private QuadraEntity quadra;
+
+    @OneToMany(mappedBy = "reserva", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<PessoaEntity> convidados;
+}
