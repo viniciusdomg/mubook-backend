c27e871909e2b3ea8615e5a9106f86a8caba491e
JoaoGuilhermeMA <joomoreira36@gmail.com>
2025-07-04 04:01:46 -0300

    refactor: refatorando o codigo do controller para que ele use o dto

--- src/main/java/br/com/mubook/mubook/restcontroller/QuadraController.java
+++ src/main/java/br/com/mubook/mubook/restcontroller/QuadraController.java
@@ -1,9 +1,11 @@
 package br.com.mubook.mubook.restcontroller;
 
+import br.com.mubook.mubook.dto.QuadraCreateDTO;
 import br.com.mubook.mubook.model.Quadra;
 import br.com.mubook.mubook.service.QuadraService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,12 +17,14 @@ public class QuadraController {
 
     private final QuadraService quadraService;
 
+    @PreAuthorize("hasRole('ADMINISTRADOR')")
     @GetMapping
     public ResponseEntity<List<Quadra>> findAll() {
         List<Quadra> lista = quadraService.findAll();
         return ResponseEntity.ok(lista);
     }
 
+    @PreAuthorize("hasRole('ADMINISTRADOR')")
     @GetMapping("/{id}")
     public ResponseEntity<Quadra> findById(@PathVariable Integer id) {
         Quadra quadra = quadraService.findById(id);
@@ -30,19 +34,21 @@ public class QuadraController {
         return ResponseEntity.ok(quadra);
     }
 
+    @PreAuthorize("hasRole('ADMINISTRADOR')")
     @PostMapping
-    public ResponseEntity<Quadra> create(@RequestBody Quadra quadra) {
-        Quadra created = quadraService.save(quadra);
+    public ResponseEntity<Quadra> create(@RequestBody QuadraCreateDTO quadraDTO) {
+        Quadra created = quadraService.createFromDTO(quadraDTO);
         return ResponseEntity.ok(created);
     }
 
+    @PreAuthorize("hasRole('ADMINISTRADOR')")
     @PutMapping("/{id}")
-    public ResponseEntity<Quadra> update(@PathVariable Integer id, @RequestBody Quadra quadra) {
-        quadra.setId(id);
-        Quadra updated = quadraService.save(quadra);
+    public ResponseEntity<Quadra> update(@PathVariable Integer id, @RequestBody QuadraCreateDTO quadraDTO) {
+        Quadra updated = quadraService.updateFromDTO(id, quadraDTO);
         return ResponseEntity.ok(updated);
     }
 
+    @PreAuthorize("hasRole('ADMINISTRADOR')")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Integer id) {
         quadraService.hardDeleteById(id);
