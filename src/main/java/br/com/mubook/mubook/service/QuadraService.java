7e7b4f6d21b55c68e5175184b427b06cdcfa6fa9
JoaoGuilhermeMA <joomoreira36@gmail.com>
2025-07-04 04:01:30 -0300

    feat: adicionando novos metodos para lidar com o dto

--- src/main/java/br/com/mubook/mubook/service/QuadraService.java
+++ src/main/java/br/com/mubook/mubook/service/QuadraService.java
@@ -1,6 +1,9 @@
 package br.com.mubook.mubook.service;
 
+import br.com.mubook.mubook.dto.QuadraCreateDTO;
 import br.com.mubook.mubook.model.Quadra;
 
 public interface QuadraService extends GenericService<Quadra, Integer> {
+    Quadra createFromDTO(QuadraCreateDTO dto);
+    Quadra updateFromDTO(Integer id, QuadraCreateDTO dto);
 }
