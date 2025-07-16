97bbe93cef17cdd58514f1ff00eac35ec8f6d9b9
Vinicius de Oliveira <viniciusmgomes2005@hotmail.com>
2025-07-15 23:15:14 -0300

    feat: corrige update do perfil

--- src/main/java/br/com/mubook/mubook/helper/PessoaHelper.java
+++ src/main/java/br/com/mubook/mubook/helper/PessoaHelper.java
@@ -1,8 +1,10 @@
 package br.com.mubook.mubook.helper;
 
 import br.com.mubook.mubook.dto.AtualizarPerfilRequest;
+import br.com.mubook.mubook.dto.PerfilResponse;
 import br.com.mubook.mubook.model.Endereco;
 import br.com.mubook.mubook.model.Pessoa;
+import br.com.mubook.mubook.model.Usuario;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
@@ -10,20 +12,34 @@ import java.time.LocalDate;
 @Component
 public class PessoaHelper {
 
-    public Pessoa RequestToPessoa(AtualizarPerfilRequest request){
+    public Pessoa RequestToPessoa(AtualizarPerfilRequest request, Pessoa pessoa){
         Endereco endereco = new Endereco();
         endereco.setCep(request.cep());
         endereco.setRua(request.endereco());
         endereco.setComplemento(request.complemento());
 
-        Pessoa pessoa = new Pessoa();
         pessoa.setNome(request.nome());
         pessoa.setCpf(request.cpf());
         pessoa.setEmail(request.email());
-        pessoa.setDataNascimento(LocalDate.parse(request.dataNascimento()));
+        pessoa.setDataNascimento(request.dataNascimento() != null ? LocalDate.parse(request.dataNascimento()) : null);
         pessoa.setGenero(request.genero());
         pessoa.setEndereco(endereco);
         pessoa.setTelefone(request.telefone());
         return pessoa;
+    }
+
+    public PerfilResponse PessoaToPerfilResponse(Usuario u){
+        Pessoa p = u.getPessoa();
+        return new PerfilResponse(p.getId(),
+                u.getFoto_url(),
+                p.getNome(),
+                p.getCpf(),
+                p.getEmail(),
+                p.getDataNascimento() != null ? p.getDataNascimento().toString() : null,
+                p.getGenero(),
+                p.getEndereco() != null ? p.getEndereco().getCep() : null,
+                p.getEndereco() != null ? p.getEndereco().getRua() : null,
+                p.getEndereco() != null ? p.getEndereco().getComplemento() : null,
+                p.getTelefone());
     }
 }
--- src/main/java/br/com/mubook/mubook/restcontroller/PerfilController.java
+++ src/main/java/br/com/mubook/mubook/restcontroller/PerfilController.java
@@ -1,14 +1,18 @@
 package br.com.mubook.mubook.restcontroller;
 
 import br.com.mubook.mubook.dto.AtualizarPerfilRequest;
+import br.com.mubook.mubook.dto.PerfilResponse;
 import br.com.mubook.mubook.helper.PessoaHelper;
-import br.com.mubook.mubook.model.Pessoa;
+import br.com.mubook.mubook.model.Usuario;
 import br.com.mubook.mubook.security.JwtService;
 import br.com.mubook.mubook.service.PessoaService;
+import br.com.mubook.mubook.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/perfil/")
@@ -17,26 +21,31 @@ public class PerfilController {
 
     private final PessoaService pessoaService;
 
+    private final UsuarioService usuarioService;
+
     private final PessoaHelper helper;
 
     private final JwtService jwtService;
 
     @GetMapping("")
     @PreAuthorize("hasAnyAuthority('ROLE_ADMINISTRADOR', 'ROLE_ASSOCIADO')")
-    public Pessoa getPerfil(@RequestHeader("Authorization") String token) {
+    public ResponseEntity<PerfilResponse> getPerfil(@RequestHeader("Authorization") String token) {
         try{
             String email = jwtService.extractUsername(token.substring(7));
-            return pessoaService.findByEmail(email);
+            Optional<Usuario> usuario = usuarioService.findByEmailOrCpf(email);
+            return usuario.map(value -> ResponseEntity.ok(helper.PessoaToPerfilResponse(value))).orElseGet(()
+                    -> ResponseEntity.internalServerError().build());
         }catch (Exception e){
-            throw new RuntimeException("Erro ao buscar dados de pessoa: "+ e.getMessage());
+            throw new RuntimeException("Erro ao atualizar perfil " + e.getMessage());
         }
     }
 
-    @PostMapping("")
+    @PutMapping("")
     @PreAuthorize("hasAnyAuthority('ROLE_ADMINISTRADOR', 'ROLE_ASSOCIADO')")
     public ResponseEntity<String> updatePerfil(@RequestBody AtualizarPerfilRequest request) {
         try{
-            var pessoa = helper.RequestToPessoa(request);
+            var pessoa = pessoaService.findById(request.id());
+            pessoa = helper.RequestToPessoa(request, pessoa);
             pessoaService.save(pessoa);
             return ResponseEntity.ok("Perfil atualizado com sucesso!");
         }catch (Exception e){
